d95bf9ea00b08fe06ef35a84f3a7d68af68847df
DamienParmenon <parmenon.damien@gmail.com>
2022-03-22 17:38:58 +0100

    Layout + css + modification vue index

---
+++ Projet2/ViewModels/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Projet2.Models;
+using System.Collections.Generic;
+
+namespace Projet2.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<Association> AssociationList = new List<Association>();
+    }
+}
