bb59a4006daf1c25c19b2369694105660362f3b0
DamienParmenon <parmenon.damien@gmail.com>
2022-03-24 16:45:20 +0100

    Ajout dashboard administrateur

---
+++ Projet2/Models/BL/Interface/IDocumentService.cs
@@ -0,0 +1,17 @@
+﻿namespace Projet2.Models.BL.Interface
+{
+    public interface IDocumentService 
+    {
+        public void CreateDocument();
+
+        public void DeleteDocument();
+
+        public string GetDocument(int id);
+
+        public string GetAssociationRepresentativeID(int AssociationId);
+
+        public string GetOfficialJournalPublication(int AssociationId);
+
+        public string GetBankDetails(int AssociationId);
+    }
+}
---
+++ Projet2/Models/Document.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Projet2.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        [DefaultValue("")]
+        public string Path { get; set; }
+
+        public int AssociationId { get; set; }
+
+        public Association Association { get; set; }
+    }
+}
