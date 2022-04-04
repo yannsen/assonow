8894820aa2e90755989e839bf22832f431750141
DamienParmenon <parmenon.damien@gmail.com>
2022-04-05 01:06:06 +0200

    test modif

--- Projet2/Models/BL/Service/AssociationEventService.cs
+++ Projet2/Models/BL/Service/AssociationEventService.cs
@@ -31,17 +31,6 @@ namespace Projet2.Models.BL.Service
         {
             int idAddress = addressService.CreateAddress(viewModel.Address);
             viewModel.AssociationEvent.AddressId = idAddress;
-
-            if (viewModel.File.Length > 0)
-            {
-                using (var ms = new MemoryStream())
-                {
-                    viewModel.File.CopyTo(ms);
-                    var fileBytes = ms.ToArray();
-                    viewModel.AssociationEvent.Image = string.Format("data:image/jpg;base64,{0}", Convert.ToBase64String(fileBytes));
-                }
-            }
-
             _bddContext.AssociationEvent.Add(viewModel.AssociationEvent);
             _bddContext.SaveChanges();
             return viewModel.AssociationEvent.Id;
